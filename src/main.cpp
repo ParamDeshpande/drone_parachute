5c61ade7f7aafc2cb9edc474dca3ccea83961f5c
ParamDeshpande <deshpandeparam10@gmail.com>
2020-01-04 19:21:49 +0530

    blink led code works

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,21 @@
-#include <iostream>
+#include "mbed.h"
+ 
+Serial      pc(PA_2, PA_3); // TX, RX
+DigitalOut  myled(PC_13);   // on-board LED
+  
+int main() 
+{  
+    while(1) {
+        // The on-board LED is connected via a resistor to +3.3V (not to GND). 
+        // So the LED is active on 0
+        myled = 0;      // turn the LED on
+        wait_ms(200);   // wait 200 millisecond
+        myled = 1;      // turn the LED off
+        pc.printf("Blink\r\n");
+        wait_ms(1000);  // wait 1000 millisecond
+    }
+}
+/*#include <iostream>
 #include "../include/global.h"
 #include "../include/battery.h"
 #include "../include/parachute.h"
@@ -25,4 +42,4 @@ void check_system(void){
     }
     
 
-}
+}*/
--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,13 @@
 #include <mbed.h>
 
+DigitalOut LEDPIN(PC_13);
 int main() {
 
   // put your setup code here, to run once:
 
   while(1) {
     // put your main code here, to run repeatedly:
+    LEDPIN != LEDPIN;
+    wait(2);
   }
 }
